6cca1cd16f3bf446a88e2492ca78ebdc6352d219
D. Paolella <dpaolell@redhat.com>
2023-09-29 15:38:24 +0200

    Optional images no longer apply

--- doozer/doozerlib/state.py
+++ doozer/doozerlib/state.py
@@ -1,7 +1,6 @@
 STATE_PEND = 'pending'
 STATE_PASS = 'passed'
 STATE_FAIL = 'failed'
-STATE_PART = 'partial'
 
 TEMPLATE_BASE_STATE = {
     'status': STATE_PASS,
@@ -14,8 +13,7 @@ TEMPLATE_IMAGE = {
     'images': {},
     'total': 0,
     'success': 0,
-    'required_fail': 0,
-    'optional_fail': 0
+    'fail': 0,
 }
 
 
@@ -30,16 +28,14 @@ def record_image_success(state, image):
 
 
 def record_image_fail(state, image, msg, logger=None):
-    state['required_fail' if image.required else 'optional_fail'] += 1
+    state['fail'] += 1
     state['images'][image.distgit_key] = msg
     if logger:
         logger.error('[{}] {}'.format(image.qualified_key, msg))
 
 
 def record_image_finish(state, msg='Complete'):
-    if state['required_fail']:
+    if state['fail']:
         state['status'] = STATE_FAIL
-    elif state['optional_fail']:
-        state['status'] = STATE_PART
     elif state['total'] == state['success']:
         state['status'] = STATE_PASS
