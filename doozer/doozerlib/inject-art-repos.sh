f688d50376c762a696a03ef3a9bee69fd060dfc1
Siddharth Sharma <sidsharm@redhat.com>
2023-12-12 22:31:20 -0500

    Modify script

--- doozer/doozerlib/inject-art-repos.sh
+++ doozer/doozerlib/inject-art-repos.sh
@@ -1,6 +1,7 @@
-if curl http://base-$major-$minor.ocp$arch_suffix.svc > /etc/yum.repos.d/art.repo ; then
+if curl "http://base-${OS_GIT_MAJOR}-${OS_GIT_MINOR}.ocp${ARCH_SUFFIX}.svc" > /etc/yum.repos.d/art.repo ; then
 	echo "Injected ART repos"
 	cat /etc/yum.repos.d/art.repo
 else
-	echo "Unable to inject ART CI repo mirror yum configuration. This is expected if you are building locally. If this is a CI triggered build, contact @release-artists in #aos-art ."
+	echo "Unable to inject ART CI repo mirror yum configuration. This is expected if you are building locally. If" \
+	"this is a CI triggered build, contact @release-artists in #forum-ocp-art ."
 fi
