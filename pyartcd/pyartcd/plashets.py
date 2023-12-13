dd94953eecfa6ff6508f351bf18b0661c0f973ee
ximhan <ximhan@redhat.com>
2023-12-13 12:45:39 +0800

    still use signing mode

--- pyartcd/pyartcd/plashets.py
+++ pyartcd/pyartcd/plashets.py
@@ -163,7 +163,7 @@ async def build_plashets(stream: str, release: str, assembly: str = 'stream',
     logger.info("Building plashet repos: %s", ", ".join(plashet_config.keys()))
 
     # Check release state
-    signing_mode = group_config['software_lifecycle']['signing_mode']
+    signing_mode = await util.get_signing_mode(group_config=group_config)
 
     # Create plashet repos on ocp-artifacts
     # We can't safely run doozer config:plashet from-tags in parallel as this moment.
