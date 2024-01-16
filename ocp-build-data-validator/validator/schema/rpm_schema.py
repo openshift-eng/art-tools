354e3ec5a1cffc4576f597c846a70b218cd57de6
Ashwin Das <ashwindasr@gmail.com>
2024-01-16 14:35:55 -0500

    migrate ocp-build-data-validator

---
+++ ocp-build-data-validator/validator/schema/rpm_schema.py
@@ -0,0 +1,74 @@
+from schema import Schema, Optional, And, Or, Regex, SchemaError
+from validator.schema.modification_schema import modification
+
+
+GIT_SSH_URL_REGEX = r'((git@[\w\.]+))([\w\.@\:/\-~]+)(\.git)(/)?'
+GIT_WEB_URL_REGEX = r'https://github.com\/[a-z-]+/[a-z-]+'
+
+valid_modes = [
+    'auto',
+    'disabled',
+    'wip',
+]
+
+RPM_CONTENT_SCHEMA = {
+    Optional('build'): {
+        'use_source_tito_config': bool,
+        'tito_target': And(str, len),
+        'push_release_commit': bool,
+    },
+    Optional('source'): {
+        Optional('alias'): And(str, len),
+        Optional('git'): {
+            'branch': {
+                Optional('fallback'): And(str, len),
+                Optional('stage'): And(str, len),
+                'target': And(str, len),
+            },
+            'url': And(str, len, Regex(GIT_SSH_URL_REGEX)),
+            'web': And(str, len, Regex(GIT_WEB_URL_REGEX)),
+        },
+        Optional('specfile'): Regex(r'.+\.spec$'),
+        Optional('modifications'): [modification],
+    },
+}
+
+rpm_schema = Schema({
+    'content': RPM_CONTENT_SCHEMA,
+    Optional('distgit'): {
+        Optional('branch'): And(str, len),
+    },
+    Optional('enabled_repos'): [
+        And(str, len),
+    ],
+    Optional('mode'): Or(*valid_modes),
+    'name': And(str, len),
+    'owners': [
+        And(str, len)
+    ],
+    Optional('maintainer'): {
+        Optional('product'): And(str, len),
+        'component': And(str, len),
+        Optional('subcomponent'): And(str, len),
+    },
+    Optional('targets'): [
+        And(str, len),
+    ],
+    Optional('hotfix_targets'): [
+        And(str, len),
+    ],
+    Optional('external_scanners'): {
+        Optional('sast_scanning'): {
+            Optional('jira_integration'): {
+                'enabled': And(bool)
+            }
+        }
+    }
+})
+
+
+def validate(_, data):
+    try:
+        rpm_schema.validate(data)
+    except SchemaError as err:
+        return '{}'.format(err)
---
+++ ocp-build-data-validator/validator/schema/streams_schema.py
@@ -0,0 +1,24 @@
+from schema import Schema, Optional, And, SchemaError
+
+STREAMS_SCHEMA = {
+        And(str, len): {
+            'image': And(str, len),
+            Optional('upstream_image_base'): And(str, len),  # won't need if we do not transform
+            'upstream_image': And(str, len),
+            Optional('mirror'): bool,
+            Optional('mirror_manifest_list'): bool,
+            Optional('transform'): And(str, len),
+            Optional('upstream_image_mirror'): list,
+        },
+    }
+
+
+def streams_schema():
+    return Schema(STREAMS_SCHEMA)
+
+
+def validate(_, data):
+    try:
+        streams_schema().validate(data)
+    except SchemaError as err:
+        return '{}'.format(err)
