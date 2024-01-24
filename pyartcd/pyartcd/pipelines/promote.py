ea546d050e4c7cf528c94251fd581be16b65ba2a
Ashwin Das <ashwindasr@gmail.com>
2024-01-24 16:23:20 -0500

    use kubeconfig

--- pyartcd/pyartcd/pipelines/promote.py
+++ pyartcd/pyartcd/pipelines/promote.py
@@ -1525,8 +1525,9 @@ class PromotePipeline:
         """
         pipeline_name = "doomsday-pipeline"
         cmd = f"tkn pipeline start {pipeline_name}" \
+              f"--kubeconfig {os.environ['ART_CLUSTER_ART_CD_PIPELINE_KUBECONFIG']}" \
               f"--param major={self.group.split('-')[-1]} " \
-              f"--param version={self.assembly}"
+              f"--param version={self.assembly} "
 
         env = os.environ.copy()
         rc, _, _ = await exectools.cmd_gather_async(cmd, env=env)
