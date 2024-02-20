7023474acc976bf9b7479e06e2a880d129fd3359
Siddharth Sharma <sidsharm@redhat.com>
2024-02-20 11:37:21 -0500

    Verify builder bug with payload

--- elliott/elliottlib/cli/get_golang_earliest_fix_cli.py
+++ elliott/elliottlib/cli/get_golang_earliest_fix_cli.py
@@ -11,6 +11,9 @@ from elliottlib.rpm_utils import parse_nvr
 from elliottlib.assembly import AssemblyTypes, assembly_type, assembly_basis_event
 from elliottlib import errata
 
+from pyartcd import constants as pyartcd_constants
+from artcommonlib.rhcos import get_container_configs
+
 
 class GetGolangEarliestFixCli:
     def __init__(self, runtime: Runtime, bug_ids: List[str]):
@@ -156,6 +159,7 @@ class GetGolangEarliestFixCli:
                 continue
 
             rpm_advisory = info['assembly'].get('group', {}).get('advisories', {}).get('rpm', 0)
+            image_advisory = info['assembly'].get('group', {}).get('advisories', {}).get('image', 0)
             if rpm_advisory not in attached_in_erratas:
                 continue
 
@@ -170,13 +174,25 @@ class GetGolangEarliestFixCli:
             fixed = self._is_fixed(bug, tracker_fixed_in, go_nvr_map)
             status = "fixed" if fixed else "not fixed"
             self._logger.info(f"{bug.id} is {status} in {assembly_name}")
-            fixed_results.append((assembly_name, rpm_advisory, fixed))
+            # append image advisory since builder bug should be associated with payload/image advisory
+            fixed_results.append((assembly_name, image_advisory, fixed))
 
         previous_fixed = None
-        for assembly_name, rpm_advisory, fixed in fixed_results[::-1]:
+        for assembly_name, image_advisory, fixed in fixed_results[::-1]:
             if fixed and not previous_fixed:
-                click.echo(f'Fix for {bug.id} was first introduced in {assembly_name}, advisory: {rpm_advisory}')
-                break
+                click.echo(f'Golang bump relevant to {bug.id} was first introduced in {assembly_name}')
+                click.echo('Verifying that payload matches golang bump')
+                pullspec = f'{pyartcd_constants.RELEASE_IMAGE_REPO}:{assembly_name}-x86_64'
+                self._logger.info(f"Fetching go build nvrs for {pullspec}...")
+                rhcos_images = {c['name'] for c in get_container_configs(self._runtime)}
+                nvr_map = await get_nvrs_from_payload(pullspec, rhcos_images)
+                nvrs = [(n, vr_tuple[0], vr_tuple[1]) for n, vr_tuple in nvr_map.items()]
+                go_nvr_map = get_golang_container_nvrs(nvrs, self._logger)
+                if self._is_fixed(bug, tracker_fixed_in, go_nvr_map):
+                    advisory_link = f'https://errata.devel.redhat.com/advisory/{image_advisory}'
+                    click.echo(f'Fix for {bug.id} was first introduced in {assembly_name} payload, and should be '
+                               f'associated with {assembly_name} release image advisory: {advisory_link}')
+                    break
             previous_fixed = fixed
 
     async def earliest_fix_rpm(self, bug, tracker_fixed_in, rpm_name):
@@ -286,19 +302,9 @@ class GetGolangEarliestFixCli:
 @click.pass_obj
 @click_coroutine
 async def get_golang_earliest_fix_cli(runtime: Runtime, bug_ids: str):
-    """Find golang tracker bugs in jira and determine if they are fixed.
-    Trackers are fetched from the OCPBUGS project that are assigned to Release component
-    Passing in an assembly is the most straightforward way to analyze golang-builder as well as rpm trackers.
-    Use --pullspec <nightly_pullspec> to determine for builds in a nightly.
-    Note: rpm trackers cannot be processed if --pullspec is used, for that rely on --assembly.
-    --update-tracker: If a tracker bug is fixed then comment on it with analysis and move the bug state to ON_QA
+    """Determine if a bug is fixed and the release, assembly, errata that it was fixed and shipped in
 
-    # Determine if currently open golang tracker bugs are fixed against 4.14.8 assembly.
-
-    $ elliott -g openshift-4.14 --assembly 4.14.8 find-bugs:golang
-
-    $ elliott -g openshift-4.14 find-bugs:golang --pullspec quay.io/openshift-release-dev/ocp-release:4.14.8-x86_64
-
+    $ elliott -g openshift-4.14 go:earliest-fix OCPBUGS-17336
     """
     # We want to load all configs for rpms, include disabled so microshift is included
     runtime.initialize(mode="rpms", disabled=True)
