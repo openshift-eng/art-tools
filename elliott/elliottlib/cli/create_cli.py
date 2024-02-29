b161facada355b677b58fcf4a32a50a726f956ce
Joep van Delft <jdelft@redhat.com>
2024-02-29 11:46:40 +0100

    Fix elliott

--- elliott/elliottlib/cli/create_cli.py
+++ elliott/elliottlib/cli/create_cli.py
@@ -1,6 +1,7 @@
 import click
 import datetime
 import elliottlib
+from artcommonlib import logutil
 from artcommonlib.assembly import AssemblyTypes
 from artcommonlib.format_util import green_prefix
 from elliottlib.cli.common import cli
@@ -12,7 +13,7 @@ from elliottlib.util import YMD, validate_release_date, \
 from elliottlib import errata
 
 
-LOGGER = elliottlib.logutil.getLogger(__name__)
+LOGGER = logutil.get_logger(__name__)
 
 
 @cli.command("create", short_help="Create a new advisory")
