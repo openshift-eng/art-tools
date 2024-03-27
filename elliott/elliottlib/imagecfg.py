8701af502a989429073155a5f89ffa3b0b857319
Yuxiang Zhu <yuxzhu@redhat.com>
2024-03-27 14:27:09 +0800

    ART-9251: olm_bundle: Fix operand name resolution
    Alternative upstream section may override image name (e.g.
    https://github.com/openshift-eng/ocp-build-data/blob/f82bb057fef0f20ab67f34b3270742b9191b6e84/images/nmstate-console-plugin.yml#L54).
    
    Currently when alternative upstream is effective, the image_name and
    image_name_short fields of ImageMetadata object don't respect the
    alternative upstream override.
    
    The root cause of this issue is that the `ImageMetadata.name` and
    `ImageMetadata.name_in_short` are not updated after an alternative
    upstream config is applied. This PR fixes this issue by changing those
    two fields to properties to make sure their values are always up to
    date.

--- elliott/elliottlib/imagecfg.py
+++ elliott/elliottlib/imagecfg.py
@@ -8,8 +8,14 @@ class ImageMetadata(Metadata):
 
     def __init__(self, runtime, data_obj):
         super(ImageMetadata, self).__init__('image', runtime, data_obj)
-        self.image_name = self.config.name
-        self.image_name_short = self.image_name.split('/')[-1]
+
+    @property
+    def image_name(self):
+        return self.config.name
+
+    @property
+    def image_name_short(self):
+        return self.image_name.split('/')[-1]
 
     @property
     def base_only(self):
