6c04f680c27baab2a12e7cad0acd615e50d7b304
Ashwin Das <ashwindasr@gmail.com>
2024-05-10 14:11:20 -0400

    [fips] fix slack message, add art-attention (#633)

--- pyartcd/pyartcd/pipelines/scan_fips.py
+++ pyartcd/pyartcd/pipelines/scan_fips.py
@@ -41,8 +41,8 @@ class ScanFips:
         if result_json:
             # alert release artists
             if not self.runtime.dry_run:
-                message = ":warning: @ashwin FIPS scan has failed for some builds. Please verify. (Triage <https://art-docs.engineering.redhat.com/sop/triage-fips|docs>)"
-                slack_response = await self.slack_client.say(message=message)
+                message = ":warning: @ashwin FIPS scan has failed for some builds. Please verify. (Triage <https://art-docs.engineering.redhat.com/sop/triage-fips/|docs>)"
+                slack_response = await self.slack_client.say(message=message, reaction=":art-attention:")
                 slack_thread = slack_response["message"]["ts"]
 
                 await self.slack_client.upload_content(
