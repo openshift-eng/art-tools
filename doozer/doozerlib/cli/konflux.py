b34aa2b8600a4b03981556b57ab9307c38d4a377
Ashwin Das <ashwindasr@gmail.com>
2024-06-05 15:25:45 -0400

    wip

---
+++ doozer/doozerlib/cli/konflux.py
@@ -0,0 +1,161 @@
+import click
+import os
+import yaml
+import jinja2
+import string
+
+from artcommonlib.exectools import cmd_assert, cmd_gather
+from doozerlib.cli import cli, pass_runtime, click_coroutine
+from doozerlib.runtime import Runtime
+
+GITHUB_TOKEN_SECRET_NAME = "github-access-token-ash"  # Secret name for GH token for openshift-priv
+QUAY_REGISTRY_REPO_URL = "quay.io/rh_ee_asdas/konflux-test"
+QUAY_REGISTRY_SECRET_NAME = "component-1"
+KONFLUX_ROOT_WORKING_DIR = "/tmp/crap/konflux"
+
+
+class Konflux:
+    def __init__(self, runtime: Runtime, distgit_name: str, namespace: str):
+        self.runtime = runtime
+
+        self.runtime.initialize(clone_distgits=False)
+        self.namespace = namespace
+
+        # An application resource name must start with a lower case alphabetical character,
+        # be under 63 characters, and can only consist of lower case alphanumeric characters or ‘-’
+        # Hence renaming openshift-4.16 to openshift-4-16 for example
+        self.major_minor = runtime.group.split("-")[-1].replace(".", "-")
+        self.application_name = runtime.group.replace(".", "-")  # Name of the Konflux application
+
+        self.distgit_name = distgit_name
+        self.image_meta = runtime.image_map[distgit_name]
+        self.component_name = f"ocp-{self.major_minor}-{distgit_name}"  # Name of the Konflux component
+        self.konflux_working_dir = f"{KONFLUX_ROOT_WORKING_DIR}/{distgit_name}"
+        self.output_registry = f"{QUAY_REGISTRY_REPO_URL}:{self.component_name}"
+
+        # https://github.com/openshift/coredns -> https://github.com/openshift-priv/coredns
+        self.github_url = self.image_meta.config["content"]["source"]["git"]["web"].replace("openshift", "openshift-priv")
+
+    @staticmethod
+    def verify_secret(secret_name: str):
+        command = f"oc get secret {secret_name}"
+        cmd_assert(command)
+
+    def initialize(self):
+        command = f"oc project {self.namespace}"
+        cmd_assert(command)
+
+        # All secret names should be unique in the namespace
+        # Check if secret for openshift-priv access is present
+        self.verify_secret(QUAY_REGISTRY_SECRET_NAME)
+
+        # Check if secret for quay registry access is present
+        self.verify_secret(GITHUB_TOKEN_SECRET_NAME)
+
+    @staticmethod
+    def generate_file(file_path: str, data: dict, output_path: str):
+        # Load the YAML template
+        with open(file_path, "r") as file:
+            template_content = file.read()
+
+        # Create a Jinja2 template from the file content
+        template = jinja2.Template(template_content)
+
+        # Render the template with data
+        rendered_content = template.render(data)
+
+        # Load the rendered content as YAML
+        yaml_data = yaml.safe_load(rendered_content)
+
+        output_directory = os.path.dirname(output_path)
+        if not os.path.exists(output_directory):
+            os.makedirs(output_directory)
+
+        # Dump the YAML data to a file
+        with open(output_path, 'w') as file:
+            yaml.safe_dump(yaml_data, file)
+
+    def get_resource_uid(self, resource_name, name):
+        command = f"oc get {resource_name} {name} -o yaml"
+        self.runtime.logger.info(f"Running command: {command}")
+        out, _ = cmd_assert(command)
+        return yaml.safe_load(out)["metadata"]["uid"]
+
+    def apply_generated_file(self, data, file_path, output_path, mode="apply"):
+        """
+        Apply the generated files to the cluster
+        """
+        self.generate_file(file_path=file_path, data=data, output_path=output_path)
+        command = f"oc {mode} -f {output_path}"
+        self.runtime.logger.info(f"Running command: {command}")
+        cmd_assert(command)
+
+    def generate_files(self):
+        data = {
+            "application": {
+                "name": self.application_name
+            },
+            "namespace": self.namespace,
+
+        }
+        self.apply_generated_file(data, "doozer/static/konflux/application.yaml",
+                                  f"{self.konflux_working_dir}/application.yaml")
+
+        application_uid = self.get_resource_uid(resource_name="Application", name=self.application_name)
+        data = {
+            "registry_details": string.Template("""'{"image":"$registry_url","visibility":"$registry_visibility","secret":"$registry_secret"}'""").substitute(registry_url=QUAY_REGISTRY_REPO_URL, registry_visibility="private", registry_secret=QUAY_REGISTRY_SECRET_NAME),
+            "namespace": self.namespace,
+            "application": {
+                "name": self.application_name,
+                "uid": application_uid
+            },
+            "component": {
+                "name": self.component_name
+            },
+            "git": {
+                "token_name": GITHUB_TOKEN_SECRET_NAME,
+                "context": "./",
+                "url": self.github_url,
+                "branch": f"art-<{self.runtime.group}>-assembly-<test>-dgk-<{self.distgit_name}>",
+                "dockerfile_path": "Dockerfile"
+            }
+        }
+        self.apply_generated_file(data, "doozer/static/konflux/component.yaml", f"{self.konflux_working_dir}/component.yaml")
+
+        component_uid = self.get_resource_uid(resource_name="Component", name=self.component_name)
+        data = {
+            "namespace": self.namespace,
+            "application": {
+                "name": self.application_name
+            },
+            "component": {
+                "name": self.component_name,
+                "uid": component_uid
+            },
+            "git": {
+                "token_name": GITHUB_TOKEN_SECRET_NAME,
+                "context": ".",
+                "url": self.github_url,
+                "branch": f"art-<{self.runtime.group}>-assembly-<test>-dgk-<{self.distgit_name}>",
+                "dockerfile_path": "Dockerfile"
+            },
+            "konflux": {
+                "bundle_sha": "quay.io/redhat-appstudio-tekton-catalog/pipeline-docker-build:13ecd03ec9f7de811f837a5460c41105231c911a"
+            },
+            "output_registry_url": self.output_registry
+        }
+
+        self.apply_generated_file(data, "doozer/static/konflux/pipeline_run.yaml", f"{self.konflux_working_dir}/pipeline_run.yaml", mode="create")
+        self.runtime.logger.info(f"Will push to registry {self.output_registry} on success")
+
+    def run(self):
+        self.initialize()
+        self.generate_files()
+
+
+@cli.command("k:ocp4", help="Konflux pipeline")
+@click.option("--namespace", required=True, help="OCP namespace where konflux is deployed to")
+@pass_runtime
+def konflux(runtime: Runtime, namespace: str):
+    pipeline = Konflux(runtime=runtime, distgit_name="coredns", namespace=namespace)
+    pipeline.run()
--- pyartcd/pyartcd/pipelines/k_ocp4.py
+++ pyartcd/pyartcd/pipelines/k_ocp4.py
@@ -1,8 +1,10 @@
 import json
 import click
 import os
+import yaml
+from artcommonlib import exectools
+from artcommonlib.util import TreeAnalyzer
 from pyartcd import constants
-from pyartcd import exectools
 from pyartcd import util
 from pyartcd.cli import cli, pass_runtime, click_coroutine
 from pyartcd.runtime import Runtime
@@ -10,9 +12,9 @@ from pyartcd.runtime import Runtime
 
 class Version:
     def __init__(self):
-        self.stream = ''  # "X.Y" e.g. "4.0"
-        self.branch = ''  # e.g. "rhaos-4.0-rhel-7"
-        self.release = ''  # e.g. "201901011200.?"
+        self.stream = ""  # "X.Y" e.g. "4.0"
+        self.branch = ""  # e.g. "rhaos-4.0-rhel-7"
+        self.release = ""  # e.g. "201901011200.?"
         self.major = 0  # X in X.Y, e.g. 4
         self.minor = 0  # Y in X.Y, e.g. 0
 
@@ -23,51 +25,84 @@ class Version:
 class KonfluxPipeline:
     def __init__(self, runtime, assembly, data_path, image_list, version, data_gitref):
         self.runtime = runtime
-        self.image_list = image_list
-        self._doozer_working = os.path.abspath(f'{self.runtime.working_dir / "doozer_working"}')
+        self.image_list = image_list.split(",")
+        self._doozer_working = os.path.abspath(f"{self.runtime.working_dir / 'doozer_working'}")
         self.version = Version()
         self.version.stream = version
         self.version.release = util.default_release_suffix()
 
-        group_param = f'--group=openshift-{version}'
+        group_param = f"--group=openshift-{version}"
         if data_gitref:
-            group_param += f'@{data_gitref}'
+            group_param += f"@{data_gitref}"
 
         self._doozer_base_command = [
-            'doozer',
-            f'--assembly={assembly}',
-            f'--working-dir={self._doozer_working}',
-            f'--data-path={data_path}',
+            "doozer",
+            f"--assembly={assembly}",
+            f"--working-dir={self._doozer_working}",
+            f"--data-path={data_path}",
             group_param
         ]
 
-    async def rebase(self):
+        self.tree_analyzer = TreeAnalyzer(self._get_tree())
+
+    async def _rebase(self, images):
         cmd = self._doozer_base_command.copy()
         cmd.extend([
-            '--latest-parent-version', f'--images={self.image_list}', 'k:images:rebase',
-            f'--version=v{self.version.stream}', f'--release={self.version.release}', '--push',
+            "--latest-parent-version", "k:images:rebase", f"--images={','.join(images)}",
+            f"--version=v{self.version.stream}", f"--release='{self.version.release}'",
             f"--message='Updating Dockerfile version and release v{self.version.stream}-{self.version.release}'",
         ])
 
         if self.runtime.dry_run:
-            cmd.append('--dry-run')
+            cmd.append("--dry-run")
 
         await exectools.cmd_assert_async(cmd)
 
+    def _get_tree(self):
+        """
+        Get the parent-tree graph from ocp-build-data, of all images
+        """
+        cmd = self._doozer_base_command.copy()
+        cmd.extend([
+            "images:show-tree",
+            "--yml"
+        ])
+        out, _ = exectools.cmd_assert(cmd)
+        self.runtime.logger.info("images:show-tree output:\n%s", out)
+        return yaml.safe_load(out)
+
+    def _get_parents_children(self, images):
+        """
+        Get the list of parents and children of a node, including the node as well.
+        """
+        unique_images = set()
+
+        for image in images:
+            unique_images.add(image)
+            parents, children = self.tree_analyzer.get_parents_and_children(image)
+            for node in parents + children:
+                unique_images.add(node)
+
+        return unique_images
+
     async def run(self):
-        await self.rebase()
+        # Get all the parents and children that we need. Combine them so that there are no duplicates.
+        # This will be the new set of images to rebase
+        parents_children = self._get_parents_children(self.image_list)
+
+        await self._rebase(images=parents_children)
 
 
 @cli.command("k_ocp4", help=" Konflux pipeline")
-@click.option('--image-list', required=True, default='',
-              help='Comma/space-separated list to include/exclude per BUILD_IMAGES '
-                   '(e.g. logging-kibana5,openshift-jenkins-2)')
-@click.option('--assembly', required=True, help='The name of an assembly to rebase & build for')
-@click.option('--data-path', required=False, default=constants.OCP_BUILD_DATA_URL,
-              help='ocp-build-data fork to use (e.g. assembly definition in your own fork)')
-@click.option('--version', required=True, help='OCP version to scan, e.g. 4.14')
-@click.option('--data-gitref', required=False, default='',
-              help='Doozer data path git [branch / tag / sha] to use')
+@click.option("--image-list", required=True, default="",
+              help="Comma/space-separated list to include/exclude per BUILD_IMAGES "
+                   "(e.g. logging-kibana5,openshift-jenkins-2)")
+@click.option("--assembly", required=True, help="The name of an assembly to rebase & build for")
+@click.option("--data-path", required=False, default=constants.OCP_BUILD_DATA_URL,
+              help="ocp-build-data fork to use (e.g. assembly definition in your own fork)")
+@click.option("--version", required=True, help="OCP version to scan, e.g. 4.14")
+@click.option("--data-gitref", required=False, default="",
+              help="Doozer data path git [branch / tag / sha] to use")
 @pass_runtime
 @click_coroutine
 async def ocp4(runtime: Runtime, assembly, data_path, image_list, version, data_gitref):
