2da0d3d7c709991a86aa04a105147992b2c245c2
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2024-06-14 12:39:20 +0200

    Fix release bumps for non ART RPMs

---
+++ pyartcd/tests/test_rebuild_golang_rpms.py
@@ -0,0 +1,39 @@
+import unittest
+from unittest import TestCase
+from unittest.mock import MagicMock
+from pyartcd.pipelines.rebuild_golang_rpms import RebuildGolangRPMsPipeline
+
+
+class TestBumpRpms(TestCase):
+
+    def setUp(self):
+        runtime = MagicMock()
+        self.pipeline = RebuildGolangRPMsPipeline(runtime, ocp_version="4.17", go_nvrs=["go1.16"], art_jira="JIRA-123")
+
+    def test_bump_up_case_1(self):
+        fake_releases = "42.rhaos4.17.abcd"
+        actual_r = self.pipeline.bump_release(fake_releases)
+        expected_r = "43.rhaos4.17.abcd"
+        self.assertEqual(actual_r, expected_r)
+
+    def test_bump_up_case_2(self):
+        fake_release = "42.1.rhaos4.17.abcd"
+        actual_r = self.pipeline.bump_release(fake_release)
+        expected_r = "43.rhaos4.17.abcd"
+        self.assertEqual(actual_r, expected_r)
+
+    def test_bump_up_case_3(self):
+        fake_releases = "rhaos4.17.abcd"
+        actual_r = self.pipeline.bump_release(fake_releases)
+        expected_r = "1.rhaos4.17.abcd"
+        self.assertEqual(actual_r, expected_r)
+
+    def test_bump_up_case_4(self):
+        fake_releases = "42.1"
+        actual_r = self.pipeline.bump_release(fake_releases)
+        expected_r = "43"
+        self.assertEqual(actual_r, expected_r)
+
+
+if __name__ == '__main__':
+    unittest.main()
