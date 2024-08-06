90d6bafd38c326eb6a67194300fcad792dc85d03
Yuxiang Zhu <yuxzhu@redhat.com>
2024-08-06 17:19:42 +0800

    Add BuildRepo class to manage local build repos

---
+++ artcommon/tests/test_git_helper.py
@@ -0,0 +1,22 @@
+import unittest
+from unittest.mock import ANY, Mock, patch
+
+
+class TestGitHelper(unittest.IsolatedAsyncioTestCase):
+    @patch('artcommonlib.exectools.cmd_assert_async')
+    async def test_run_git(self, cmd_assert_async: Mock):
+        # Test that run_git calls exectools.cmd_assert_async with the correct arguments
+        from artcommonlib import git_helper
+        args = ["init", "local_dir"]
+        check = True
+        await git_helper.run_git(args, check=check)
+        cmd_assert_async.assert_called_once_with(["git"] + args, env=ANY, check=check)
+
+    @patch('artcommonlib.exectools.cmd_gather_async')
+    async def test_gather_git(self, cmd_gather_async: Mock):
+        # Test that gather_git calls exectools.cmd_gather_async with the correct arguments
+        from artcommonlib import git_helper
+        args = ["init", "local_dir"]
+        check = True
+        await git_helper.gather_git(args, check=check)
+        cmd_gather_async.assert_called_once_with(["git"] + args, env=ANY, check=check)
