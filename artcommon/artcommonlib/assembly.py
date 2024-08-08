c9b315aabf3b27937450d4189f48b3c601924ae6
Ashwin Das <ashwindasr@gmail.com>
2024-08-08 17:07:42 -0400

    allow CONFLICTING_INHERITED_DEPENDENCY temporarily

--- artcommon/artcommonlib/assembly.py
+++ artcommon/artcommonlib/assembly.py
@@ -231,7 +231,7 @@ def assembly_permits(releases_config: Model, assembly: typing.Optional[str]) -> 
 
     # Do some basic validation here to fail fast
     if assembly_type(releases_config, assembly) == AssemblyTypes.STANDARD:
-        if any(permit.code != AssemblyIssueCode.EMBARGOED_CONTENT.name for permit in defined_permits):
+        if any(permit.code not in [AssemblyIssueCode.EMBARGOED_CONTENT.name, AssemblyIssueCode.CONFLICTING_INHERITED_DEPENDENCY.name] for permit in defined_permits):
             raise ValueError(f'STANDARD assemblies like {assembly} only allow {AssemblyIssueCode.EMBARGOED_CONTENT.name} in "permits"')
 
     for permit in defined_permits:
