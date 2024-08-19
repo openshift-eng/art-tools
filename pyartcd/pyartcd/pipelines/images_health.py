9e3e2e451cad4f90b58498f4f7fabdb2147e0570
D. Paolella <dpaolell@redhat.com>
2024-08-19 10:41:38 +0200

    #aos-art has been renamed into #forum-ocp-art

--- pyartcd/pyartcd/pipelines/images_health.py
+++ pyartcd/pyartcd/pipelines/images_health.py
@@ -7,13 +7,13 @@ from pyartcd.runtime import Runtime
 
 class ImagesHealthPipeline:
     def __init__(self, runtime: Runtime, version: str,
-                 send_to_release_channel: bool, send_to_aos_art: bool):
+                 send_to_release_channel: bool, send_to_forum_ocp_art: bool):
         self.runtime = runtime
         self.doozer_working = self.runtime.working_dir / "doozer_working"
         self.logger = self.runtime.logger
         self.version = version
         self.send_to_release_channel = send_to_release_channel
-        self.send_to_aos_art = send_to_aos_art
+        self.send_to_forum_ocp_art = send_to_forum_ocp_art
         self.report = ''
         self.slack_client = None
 
@@ -35,7 +35,7 @@ class ImagesHealthPipeline:
         if self.report:
             self.logger.info('images:health output for openshift-%s:\n%s', self.version, out)
 
-        if any([self.send_to_release_channel, self.send_to_aos_art]):
+        if any([self.send_to_release_channel, self.send_to_forum_ocp_art]):
             self.slack_client = self.runtime.new_slack_client()
             await self._send_notifications()
 
@@ -49,7 +49,7 @@ class ImagesHealthPipeline:
                 response = await self.slack_client.say(msg)
                 await self.slack_client.say(f'{self.report}', thread_ts=response['ts'])
 
-            if self.send_to_aos_art:
+            if self.send_to_forum_ocp_art:
                 self.slack_client.bind_channel('#forum-ocp-art')
                 response = await self.slack_client.say(msg)
                 await self.slack_client.say(f'{self.report}', thread_ts=response['ts'])
@@ -64,9 +64,9 @@ class ImagesHealthPipeline:
 @click.option('--version', required=True, help='OCP version to scan')
 @click.option('--send-to-release-channel', is_flag=True,
               help='If true, send output to #art-release-4-<version>')
-@click.option('--send-to-aos-art', is_flag=True,
+@click.option('--send-to-forum-ocp-art', is_flag=True,
               help='"If true, send notification to #forum-ocp-art')
 @pass_runtime
 @click_coroutine
-async def images_health(runtime: Runtime, version: str, send_to_release_channel: bool, send_to_aos_art: bool):
-    await ImagesHealthPipeline(runtime, version, send_to_release_channel, send_to_aos_art).run()
+async def images_health(runtime: Runtime, version: str, send_to_release_channel: bool, send_to_forum_ocp_art: bool):
+    await ImagesHealthPipeline(runtime, version, send_to_release_channel, send_to_forum_ocp_art).run()
