8783a85d51d4d239c76fe809d29cdf64ad3ae701
Siddharth Sharma <sidsharm@redhat.com>
2024-10-01 12:04:25 -0400

    Search builds by engine

--- elliott/elliottlib/cli/find_konflux_builds_cli.py
+++ elliott/elliottlib/cli/find_konflux_builds_cli.py
@@ -1,7 +1,7 @@
 import click
 
 from artcommonlib import logutil
-from artcommonlib.konflux.konflux_build_record import KonfluxBuildOutcome
+from artcommonlib.konflux.konflux_build_record import KonfluxBuildOutcome, Engine, ArtifactType
 from elliottlib import Runtime
 from elliottlib.cli.common import cli, pass_runtime, click_coroutine
 
@@ -11,23 +11,31 @@ LOGGER = logutil.get_logger(__name__)
 @cli.command('find-k-builds', short_help='Find Konflux builds')
 @click.option(
     '--kind', '-k', metavar='KIND', required=True,
-    type=click.Choice(['rpm', 'image']),
-    help='Find builds of the given KIND [rpm, image]')
+    type=click.Choice([e.value for e in ArtifactType]),
+    help=f'Find builds of the given KIND {[e.value for e in ArtifactType]}')
+@click.option(
+    '--engine', '-e', metavar='ENGINE', required=False,
+    type=click.Choice([e.value for e in Engine]),
+    help=f'Limit builds to ones built in the given ENGINE {[e.value for e in Engine]}')
 @pass_runtime
 @click_coroutine
-async def find_k_builds_cli(runtime: Runtime, kind):
+async def find_k_builds_cli(runtime: Runtime, kind, engine):
     runtime.initialize(mode='images' if kind == 'image' else 'rpms')
     if runtime.konflux_db is None:
         raise RuntimeError('Must run Elliott with Konflux DB initialized')
 
-    LOGGER.info('searching for builds of kind %s in group %s', kind, runtime.group)
+    engine_fmt = f"{engine} " if engine else ""
+    LOGGER.info(f'searching for {engine_fmt}builds of kind %s in group %s', kind, runtime.group)
     metas = runtime.image_metas() if kind == 'image' else runtime.rpm_metas()
     names = [meta.name for meta in metas]
-    builds = await runtime.konflux_db.get_latest_builds(
-        names=names,
-        group=runtime.group,
-        outcome=KonfluxBuildOutcome.SUCCESS
-    )
+    args = {
+        'names': names,
+        'group': runtime.group,
+        'outcome': KonfluxBuildOutcome.SUCCESS
+    }
+    if engine:
+        args.update({'engine': engine})
+    builds = await runtime.konflux_db.get_latest_builds(**args)
 
     missing_builds = [name for name in names if name not in [b.name for b in builds]]
     if missing_builds:
