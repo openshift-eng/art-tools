9af3be90cf8489eef07b72f5ef8870caf9b97bcd
Adam Dobes <124796824+adobes1@users.noreply.github.com>
2024-10-18 19:13:12 +0200

    [art-cluster] quay backup override arches to sync from quay for 4.12 releases (#1049)
    * Override arches to sync from quay for 4.12 releases
    
    * Remove --all-arches option
    
    * create emtpy config file
    
    * use latest image
    
    * improve logs
    
    ---------
    
    Co-authored-by: Ashwin Das <ashwindasr@gmail.com>

--- pyartcd/pyartcd/pipelines/quay_doomsday_backup.py
+++ pyartcd/pyartcd/pipelines/quay_doomsday_backup.py
@@ -11,6 +11,7 @@ from doozerlib.util import mkdirs
 
 
 N_RETRIES = 4
+ALL_ARCHES_LIST = ["x86_64", "s390x", "ppc64le", "aarch64", "multi"]
 
 
 class QuayDoomsdaySync:
@@ -18,23 +19,15 @@ class QuayDoomsdaySync:
     Backup promoted payloads to AWS bucket, in the event we need to recover a payload or if quay goes down
     """
 
-    ALL_ARCHES_LIST = ["x86_64", "s390x", "ppc64le", "aarch64", "multi"]
-
-    def __init__(self, runtime: Runtime, all_arches: bool, arches: Optional[list[str]], version: str):
+    def __init__(self, runtime: Runtime, version: str, arches: Optional[str]):
         self.runtime = runtime
         self.version = version
         self.workdir = "./workspace"
 
-        if arches and all_arches:
-            raise Exception("Cannot specify both --arches and --all-arches")
-
-        if not (arches or all_arches):
-            raise Exception("Either --arches or --all-arches needs to be specified")
-
-        self.arches = arches.split(",") if not all_arches else self.ALL_ARCHES_LIST
+        self.arches = arches.split(",") if arches else ALL_ARCHES_LIST
 
     def sync_arch(self, arch: str):
-        if arch not in self.ALL_ARCHES_LIST:
+        if arch not in ALL_ARCHES_LIST:
             raise Exception(f"Invalid arch: {arch}")
 
         major_minor = ".".join(self.version.split(".")[:2])
@@ -51,11 +44,13 @@ class QuayDoomsdaySync:
             f"s3://ocp-doomsday-registry/release-image/{path}"
         ]
 
+        self.runtime.logger.info("Running mirror command: %s", mirror_cmd)
         cmd_assert(mirror_cmd, realtime=True, retries=N_RETRIES)
         if self.runtime.dry_run:
             self.runtime.logger.info("[DRY RUN] Would have run %s", " ".join(aws_cmd))
         else:
             sleep(5)
+            self.runtime.logger.info("Running aws command: %s", aws_cmd)
             cmd_assert(aws_cmd, realtime=True, retries=N_RETRIES)
             sleep(5)
 
@@ -71,14 +66,17 @@ class QuayDoomsdaySync:
             self.sync_arch(arch)
 
 
-@cli.command("quay-doomsday-backup", help="Run doomsday pipeline for the specified version and arches")
+@cli.command("quay-doomsday-backup", help="Run doomsday pipeline for the specified version and all arches unless --arches is specified")
 @click.option("--arches", required=False, help="Comma separated list of arches to sync")
-@click.option("--all-arches", is_flag=True, required=False, default=False, help="Sync all arches")
 @click.option("--version", required=True, help="Release to sync, e.g. 4.15.3")
 @pass_runtime
-def quay_doomsday_backup(runtime: Runtime, arches: str, all_arches: bool, version: str):
+def quay_doomsday_backup(runtime: Runtime, arches: str, version: str):
+
+    # In 4.12 we sync only x86_64 and s390x
+    if version.startswith("4.12"):
+        arches = "x86_64,s390x"
+
     doomsday_pipeline = QuayDoomsdaySync(runtime=runtime,
                                          arches=arches,
-                                         all_arches=all_arches,
                                          version=version)
     doomsday_pipeline.run()
