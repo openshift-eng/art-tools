8a63c374129f97ca4c8e1c73682157c783d72042
Ashwin Das <ashwindasr@gmail.com>
2024-12-10 12:59:12 -0500

    [konflux] use lru cache

--- doozer/doozerlib/backend/konflux_client.py
+++ doozer/doozerlib/backend/konflux_client.py
@@ -7,6 +7,7 @@ import requests
 from artcommonlib import exectools
 from artcommonlib import util as art_util
 from async_lru import alru_cache
+from functools import lru_cache
 from kubernetes import config, watch
 from kubernetes.client import ApiClient, Configuration
 from kubernetes.dynamic import DynamicClient, exceptions, resource
@@ -296,8 +297,15 @@ class KonfluxClient:
         if additional_tags is None:
             additional_tags = []
         https_url = art_util.convert_remote_git_to_https(git_url)
-        response = requests.get(constants.KONFLUX_PlR_TEMPLATE_URL)
-        template = jinja2.Template(response.text, autoescape=True)
+
+        @lru_cache()
+        def _get_plr_template():
+            response = requests.get(constants.KONFLUX_PlR_TEMPLATE_URL)
+
+            return response.text
+
+        plr_template = _get_plr_template()
+        template = jinja2.Template(plr_template, autoescape=True)
         rendered = template.render({
             "source_url": https_url,
             "revision": commit_sha,
