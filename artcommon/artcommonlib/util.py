dc606e6f6bf803e8382d6e38381c1eb1561f350c
Yuxiang Zhu <yuxzhu@redhat.com>
2024-12-12 18:23:06 +0800

    Konflux: Add command to rebase/build bundles
    Test command:
    
    ```
    doozer --assembly=test --group=openshift-4.18 --latest-parent-version beta:images:konflux:bundle --konflux-kubeconfig=/path/to/kubeconfig -
    -konflux-namespace=ocp-art-tenant --image-repo=quay.io/openshift-release-dev/ocp-v4.0-art-dev-test ose-metallb-operator-container-v4.18.0-202410251504.p0.gc
    5a3b07.assembly.test.el9
    ```

--- artcommon/artcommonlib/util.py
+++ artcommon/artcommonlib/util.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 import logging
 from typing import OrderedDict, Optional, Tuple, Iterable, List
 from datetime import datetime, timezone, timedelta, date
@@ -53,6 +54,7 @@ def new_roundtrip_yaml_handler():
     return yaml
 
 
+@lru_cache(maxsize=512)
 def convert_remote_git_to_https(source_url: str):
     """
     Accepts a source git URL in ssh or https format and return it in a normalized
@@ -77,6 +79,7 @@ def convert_remote_git_to_https(source_url: str):
     return f'https://{server}/{org_repo}'
 
 
+@lru_cache(maxsize=512)
 def convert_remote_git_to_ssh(url):
     """
     Accepts a remote git URL and turns it into a git@
