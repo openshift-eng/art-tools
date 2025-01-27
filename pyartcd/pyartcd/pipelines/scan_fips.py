d7b34126037610df55e9281afe712fbd5cc61492
Adam Dobes <adobes@redhat.com>
2025-01-27 13:26:13 +0100

    Report FIPS pipeline results to Slack

--- pyartcd/pyartcd/pipelines/scan_fips.py
+++ pyartcd/pyartcd/pipelines/scan_fips.py
@@ -5,13 +5,25 @@ This job is deployed on ART cluster
 
 import json
 import sys
+import re
+from collections import defaultdict
+import os
 
 import click
 from typing import Optional
 from pyartcd.runtime import Runtime
+from pyartcd.jira import JIRAClient
 from pyartcd.cli import cli, pass_runtime, click_coroutine
 from artcommonlib.constants import ACTIVE_OCP_VERSIONS
 from artcommonlib import exectools
+
+
+JIRA_PROJECT = "OCPBUGS"
+JIRA_DOMAIN = "https://issues.redhat.com/"
+
+INITIAL_SLACK_MSG = ":warning: FIPS scan has failed for some builds. Please verify (Triage <https://art-docs.engineering.redhat.com/sop/triage-fips/|docs>)"
+FAILING_BUILDS_MSG_HEADER = "The listed versions of the following packages did not pass the FIPS scan:"
+PACKAGES_WITHOUT_TICKET_MSG_HEADER = "Report of missing/incomplete Jira tickets:"
 
 
 class ScanFips:
@@ -21,12 +33,99 @@ class ScanFips:
         self.nvrs = nvrs
         self.all_images = all_images
 
+        #  Call JIRAClient.from_url() directly because Runtime.new_jira_client() does not work currently
+        self.jira_client = JIRAClient.from_url(server_url=JIRA_DOMAIN, token_auth=os.environ["JIRA_TOKEN"])
+
         # Setup slack client
         self.slack_client = self.runtime.new_slack_client()
         self.slack_client.bind_channel("#art-release")
 
+    @staticmethod
+    def extract_package_name(nvr: str) -> Optional[str]:
+        match = re.match(r"(.+)-v4\..*", nvr)
+        return match.group(1) if match else None
+
+    @staticmethod
+    def construct_failing_packages_report(failing_packages: dict[str, set[str]], ticket_mapping: dict[str, tuple[str, set[str]]]) -> str:
+        """
+        Construct Slack message describing packages failing the FIPS scan and their versions
+        """
+        report_msg_parts = [FAILING_BUILDS_MSG_HEADER]
+
+        for package, versions in failing_packages.items():
+            if package not in ticket_mapping:
+                report_msg_parts.append(f"`{package}` (No ticket raised)")
+            else:
+                ticket_url, _ = ticket_mapping[package]
+                report_msg_parts.append(f"`{package}` (<{ticket_url}|Ticket> already raised)")
+            report_msg_parts.append(f"• {', '.join(versions)}")
+            report_msg_parts.append("\n")
+
+        return "\n".join(report_msg_parts)
+
+    @staticmethod
+    def construct_packages_without_ticket_report(failing_packages: dict[str, set[str]], ticket_mapping: dict[str, tuple[str, set[str]]]) -> str:
+        """
+        Construct Slack message showing which of the failing builds do not have
+        an active JIRA ticket
+        """
+        report_msg_parts = [PACKAGES_WITHOUT_TICKET_MSG_HEADER]
+
+        for package in failing_packages:
+            if package not in ticket_mapping:
+                report_msg_parts.append(f"• `{package}` does not have a corresponding active ticket")
+            else:
+                ticket_url, ticket_versions = ticket_mapping[package]
+                versions_not_in_ticket = failing_packages[package].difference(ticket_versions)
+                if not versions_not_in_ticket:
+                    continue
+                use_plural = len(versions_not_in_ticket) > 1
+                report_msg_parts.append(f"• {'Versions' if use_plural else 'Version'} {', '.join(versions_not_in_ticket)} of `{package}` {'are' if use_plural else 'is'} not included in the <{ticket_url}|ticket>")
+
+        return "\n".join(report_msg_parts) if len(report_msg_parts) > 1 else ""
+
+    def get_ticket_mapping(self, failing_packages: dict[str, set[str]]) -> dict[str, tuple[str, set[str]]]:
+        """
+        Returns a dict of (failing) packages with the URL and affected versions of their
+        corresponding active tickets
+        """
+        def get_active_fips_tickets(self):
+            query = f'project = "{JIRA_PROJECT}" AND labels = "art:fips" \
+            AND status in ("New", "Modified", "ASSIGNED", "POST")'
+            active_tickets = self.jira_client._client.search_issues(jql_str=query)
+            return active_tickets
+
+        active_fips_tickets = get_active_fips_tickets(self)
+        ticket_mapping = {}
+
+        for ticket in active_fips_tickets:
+            for package in failing_packages:
+                if f"art:package:{package}" in ticket.fields.labels:
+                    ticket_versions = set(map(lambda version: version.name[:-2], ticket.fields.versions))
+                    ticket_mapping[package] = (f"{JIRA_DOMAIN}/browse/{ticket.key}", ticket_versions)
+                    break  # Assumes there's only one ticket per package
+
+        return ticket_mapping
+
+    async def message_on_slack(self, failing_packages: dict[str, set[str]]):
+        """
+        Send 2 messages to a new Slack thread - report of packages (and their versions)
+        that are currently failing the FIPS scan, and a report of packages which don't have
+        a jira ticket raised (or their ticket doesn't list all the versions currently failing)
+        """
+        ticket_mapping = self.get_ticket_mapping(failing_packages)
+
+        failing_packages_report_msg = self.construct_failing_packages_report(failing_packages, ticket_mapping)
+        packages_without_ticket_report_msg = self.construct_packages_without_ticket_report(failing_packages, ticket_mapping)
+
+        await self.slack_client.say_in_thread(message=INITIAL_SLACK_MSG)
+        await self.slack_client.say_in_thread(message=failing_packages_report_msg)
+        if packages_without_ticket_report_msg:
+            await self.slack_client.say_in_thread(message=packages_without_ticket_report_msg)
+
     async def run(self):
         results = {}
+        failing_packages = defaultdict(set)
 
         for version in ACTIVE_OCP_VERSIONS:
             cmd = [
@@ -53,6 +152,10 @@ class ScanFips:
                 result_json = json.loads(result)
                 results.update(result_json)
 
+                package_names = map(lambda nvr: self.extract_package_name(nvr), list(result_json.keys()))
+                for package_name in package_names:
+                    failing_packages[package_name].add(version)
+
             if self.all_images:
                 # Clean all the images, if we are checking for all images since this mode is used on prod only
                 # Since this command will be run for all versions, clean after each run will be more efficient. Otherwise
@@ -62,22 +165,12 @@ class ScanFips:
 
         self.runtime.logger.info(f"Result: {results}")
 
-        if results:
-            # alert release artists
+        if failing_packages:
+            # Post on Slack
             if not self.runtime.dry_run:
-                message = ":warning: FIPS scan has failed for some builds. Please verify. (Triage <https://art-docs.engineering.redhat.com/sop/triage-fips/|docs>)"
-                slack_response = await self.slack_client.say(message=message)
-                slack_thread = slack_response["message"]["ts"]
-
-                await self.slack_client.upload_content(
-                    content=json.dumps(results),
-                    filename="report.json",
-                    thread_ts=slack_thread,
-                )
-
                 # Exit as error so that we see in the PipelineRun
                 self.runtime.logger.error("FIPS issues were found")
-
+                await self.message_on_slack(failing_packages)
             else:
                 self.runtime.logger.info("[DRY RUN] Would have messaged slack")
             sys.exit(1)
