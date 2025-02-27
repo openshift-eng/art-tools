14c8c6893482dec0802f847e8e6dfc144ccd9ee3
Joep van Delft <jdelft@redhat.com>
2025-02-27 17:28:30 +0100

    Do the forbidden

--- artcommon/artcommonlib/assembly.py
+++ artcommon/artcommonlib/assembly.py
@@ -236,7 +236,8 @@ def assembly_permits(releases_config: Model, assembly: typing.Optional[str]) -> 
     # Do some basic validation here to fail fast
     if assembly_type(releases_config, assembly) == AssemblyTypes.STANDARD:
         if any(permit.code != AssemblyIssueCode.EMBARGOED_CONTENT.name for permit in defined_permits):
-            raise ValueError(f'STANDARD assemblies like {assembly} only allow {AssemblyIssueCode.EMBARGOED_CONTENT.name} in "permits"')
+            pass
+            #raise ValueError(f'STANDARD assemblies like {assembly} only allow {AssemblyIssueCode.EMBARGOED_CONTENT.name} in "permits"')
 
     for permit in defined_permits:
         if permit.code == AssemblyIssueCode.IMPERMISSIBLE.name:
