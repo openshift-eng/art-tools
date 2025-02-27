e74037bcc9b788cb2c20b00d57d62f7bb01017d9
Ximin han <1921745321@qq.com>
2025-02-27 10:46:50 +0800

    Revert "ART-9439 - modifications in prepare-release"

--- doozer/doozerlib/cli/release_gen_payload.py
+++ doozer/doozerlib/cli/release_gen_payload.py
@@ -159,14 +159,10 @@ def default_imagestream_base_name(version: str, runtime: Runtime) -> str:
 
 def assembly_imagestream_base_name(runtime: Runtime) -> str:
     version = runtime.get_minor_version()
-    return assembly_imagestream_base_name_generic(version, runtime.assembly, runtime.assembly_type)
-
-
-def assembly_imagestream_base_name_generic(version, assembly_name, assembly_type):
-    if assembly_name == 'stream' and assembly_type is AssemblyTypes.STREAM:
-        return default_imagestream_base_name(version)
+    if runtime.assembly == 'stream' and runtime.assembly_type is AssemblyTypes.STREAM:
+        return default_imagestream_base_name(version, runtime)
     else:
-        return f"{version}-art-assembly-{assembly_name}"
+        return f"{version}-art-assembly-{runtime.assembly}"
 
 
 def default_imagestream_namespace_base_name() -> str:
