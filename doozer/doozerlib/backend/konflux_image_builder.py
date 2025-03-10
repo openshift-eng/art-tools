4fe6fb95cb09808dec49c93354fd1a5ff783a141
Ashwin Das <ashwindasr@gmail.com>
2025-03-10 12:43:29 -0400

    test

--- doozer/doozerlib/backend/konflux_image_builder.py
+++ doozer/doozerlib/backend/konflux_image_builder.py
@@ -225,8 +225,8 @@ class KonfluxImageBuilder:
 
         for package_manager in ["gomod", "npm", "pip"]:
             if package_manager in required_package_managers:
-                entries: dict = metadata.config.cachito.packages.get(package_manager, [])
-                logger.info(f"entries for {package_manager}: {entries}")
+                entries: dict = metadata.config.cachito.packages.primitive()
+                logger.info(f"entries: {entries}")
 
                 flag = False
                 data = {"type": package_manager}
