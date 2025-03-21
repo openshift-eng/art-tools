4ee6a2024ca0d4207955bf342d859722c80d82b1
Siddharth Sharma <sidsharm@redhat.com>
2025-03-21 14:17:14 -0400

    fix lint

--- ocp-build-data-validator/validator/schema/shipment_schema.py
+++ ocp-build-data-validator/validator/schema/shipment_schema.py
@@ -12,11 +12,12 @@ else:
     # importlib.resources has files(), so use that:
     import importlib.resources as importlib_resources
 
+
 def validate(_, data):
     # Load Json schemas
     path = importlib_resources.files("validator") / "json_schemas"
-    schemas = {source.name: json.load(open(source)) for source in path.iterdir() if source.name.endswith(".json") and
-               source.name.startswith("shipment")}
+    schemas = {source.name: json.load(open(source)) for source in path.iterdir()
+               if source.name.endswith(".json") and source.name.startswith("shipment")}
     schema_store = {schema.get("$id", filename): schema for filename, schema in schemas.items()}
     schema = schema_store["shipment.schema.json"]
     resolver = RefResolver.from_schema(schema, store=schema_store)
