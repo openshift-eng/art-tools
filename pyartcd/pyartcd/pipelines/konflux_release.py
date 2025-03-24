7f270a7472e5448af4ca04992bc202bfb976a4ca
Siddharth Sharma <sidsharm@redhat.com>
2025-03-24 11:04:06 -0400

    tweaks

--- pyartcd/pyartcd/pipelines/konflux_release.py
+++ pyartcd/pyartcd/pipelines/konflux_release.py
@@ -3,7 +3,7 @@ from ruamel.yaml import YAML
 
 from pyartcd.cli import cli, click_coroutine, pass_runtime
 from pyartcd.runtime import Runtime
-from artcommonlib.constants import KONFLUX_RELEASE_DATA_URL
+from artcommonlib.constants import SHIPMENT_DATA_URL
 from artcommonlib import exectools, logutil
 
 yaml = YAML()
@@ -21,23 +21,19 @@ class KonfluxReleasePipeline:
                  runtime: Runtime,
                  group: str,
                  assembly: str,
-                 konflux_release_path: str,
-                 config_filename: str,
+                 shipment_path: str,
+                 config: str,
                  release_env: str,
                  force: bool):
         self.runtime = runtime
         self.group = group
         self.assembly = assembly
         self.force = force
-        self.konflux_release_path = konflux_release_path
-        self.config_filename = config_filename
+        self.shipment_path = shipment_path
+        self.config = config
         self.release_env = release_env
 
         self._working_dir = self.runtime.working_dir.absolute()
-
-        if not self.konflux_release_path:
-            self.konflux_release_path = KONFLUX_RELEASE_DATA_URL
-
         self.dry_run = self.runtime.dry_run
         self.elliott_working_dir = self._working_dir / "elliott-working"
 
@@ -46,8 +42,9 @@ class KonfluxReleasePipeline:
             f'--group={self.group}',
             f'--assembly={self.assembly}',
             f'--working-dir={self.elliott_working_dir}',
-            f'--konflux-release-path={self.konflux_release_path}',
         ]
+        if self.shipment_path:
+            self._elliott_base_command.append(f'--shipment-path={self.shipment_path}')
 
     async def run(self):
         release_name = await self.create_release()
@@ -57,10 +54,9 @@ class KonfluxReleasePipeline:
         cmd = self._elliott_base_command + [
             "release",
             "new",
-            self.release_env,
+            f"--env={self.release_env}",
+            f"--config={self.config}"
         ]
-        if self.config_filename:
-            cmd.append(f"--filename={self.config_filename}")
         if self.force:
             cmd.append("--force")
         if not self.dry_run:
@@ -84,23 +80,30 @@ class KonfluxReleasePipeline:
 
 
 @cli.command("konflux-release")
-@click.option("--konflux-release-path", metavar='KONFLUX_RELEASE_PATH', default=None,
-              help="Git repo or directory containing group metadata for konflux release e.g."
-                   f" {KONFLUX_RELEASE_DATA_URL}. Defaults to the default branch of the repo - to point to a "
-                   f"branch/commit use repo@commitish")
+@click.option("--shipment-path", metavar='SHIPMENT_PATH', default=None,
+              help="Git repo or directory containing group metadata for shipping a konflux release e.g."
+                   f" {SHIPMENT_DATA_URL.format('ocp')}. Defaults to `main` branch for a repo - to point to a "
+                   "different branch/commit use repo@commitish")
 @click.option("-g", "--group", metavar='GROUP', required=True,
               help="The group to operate on. e.g. openshift-4.18")
 @click.option("--assembly", metavar="ASSEMBLY", required=True,
               help="The name of the associated assembly e.g. 4.18.1")
-@click.option("--config-filename", metavar="CONFIG_FILENAME",
-              help="(Optional) Release config filename to use. Defaults to assembly name")
-@click.argument('env', metavar='RELEASE_ENV', nargs=1, required=True, type=click.Choice(["stage", "prod"]))
-@click.option('--force', is_flag=True, help='Proceed even if an associated release/advisory detected')
+@click.option("--config", metavar="CONFIG_PATH", required=True,
+              help="Path of the shipment config file to use for creating release. The path should be from the root of the  "
+                   "given shipment-data repo.")
+@click.option('--env', metavar='RELEASE_ENV', required=True, type=click.Choice(["stage", "prod"]),
+              help='Release environment to create the release for')
+@click.option('--force', is_flag=True,
+              help='Proceed even if an associated release/advisory detected')
 @pass_runtime
 @click_coroutine
-async def konflux_release(runtime: Runtime, konflux_release_path: str, group: str, assembly: str, config_filename: str,
+async def konflux_release(runtime: Runtime, shipment_path: str, group: str, assembly: str, config: str,
                           env: str, force: bool):
-    pipeline = KonfluxReleasePipeline(runtime=runtime, konflux_release_path=konflux_release_path, group=group,
-                                      assembly=assembly, config_filename=config_filename,
-                                      release_env=env, force=force)
+    pipeline = KonfluxReleasePipeline(runtime=runtime,
+                                      shipment_path=shipment_path,
+                                      group=group,
+                                      assembly=assembly,
+                                      config=config,
+                                      release_env=env,
+                                      force=force)
     await pipeline.run()
