24f2a0ec9cba39d62c3ab189be39eac4913db43a
Siddharth Sharma <sidsharm@redhat.com>
2025-04-07 14:29:04 -0400

    sync changes

--- ocp-build-data-validator/validator/schema/shipment_schema.py
+++ ocp-build-data-validator/validator/schema/shipment_schema.py
@@ -13,43 +13,18 @@ else:
     import importlib.resources as importlib_resources
 
 
-class ShipmentValidator:
-    def __init__(self):
-        path = importlib_resources.files("validator") / "json_schemas"
-        schemas = {source.name: json.load(open(source)) for source in path.iterdir()
-                   if source.name.endswith(".json") and source.name.startswith("shipment")}
-        schema_store = {schema.get("$id", filename): schema for filename, schema in schemas.items()}
-        self.schema = schema_store["shipment.schema.json"]
-        self.resolver = RefResolver.from_schema(self.schema, store=schema_store)
-        self.validator = validator_for(self.schema)(self.schema, resolver=self.resolver)
-        self.full_schema = self.resolve_schema(self.schema)
-
-    def resolve_schema(self, schema):
-        def resolve_ref(ref_schema):
-            """Resolve schema references."""
-            if "$ref" in ref_schema:
-                ref = ref_schema["$ref"]
-                with self.resolver.resolving(ref) as resolved:
-                    return resolved
-            return ref_schema
-
-        resolved_schema = resolve_ref(schema)
-        if not (schema.get("type") == "object" and "properties" in schema):
-            return resolved_schema
-
-        resolved_schema["properties"] = {
-            prop_name: self.resolve_schema(prop_schema)
-            for prop_name, prop_schema in resolved_schema["properties"].items()
-        }
-        return resolved_schema
-
-    def validate(self, data):
-        try:
-            self.validator.validate(data)
-        except ValidationError:
-            errors = self.validator.iter_errors(data)
-            return '\n'.join([f"{e.json_path}: {e.message}" for e in errors])
-
-
 def validate(_, data):
-    return ShipmentValidator().validate(data)
+    # Load Json schemas
+    path = importlib_resources.files("validator") / "json_schemas"
+    schemas = {source.name: json.load(open(source)) for source in path.iterdir()
+               if source.name.endswith(".json") and source.name.startswith("shipment")}
+    schema_store = {schema.get("$id", filename): schema for filename, schema in schemas.items()}
+    schema = schema_store["shipment.schema.json"]
+    resolver = RefResolver.from_schema(schema, store=schema_store)
+    validator = validator_for(schema)(schema, resolver=resolver)
+    # Validate with JSON schemas
+    try:
+        validator.validate(data)
+    except ValidationError:
+        errors = validator.iter_errors(data)
+        return '\n'.join([f"{e.json_path}: {e.message}" for e in errors])
