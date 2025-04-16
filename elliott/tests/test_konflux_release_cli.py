74a14157f141fc7716e8365383c203c701dbf422
Siddharth Sharma <sidsharm@redhat.com>
2025-04-15 23:08:47 -0400

    add more tests

--- elliott/tests/test_konflux_release_cli.py
+++ elliott/tests/test_konflux_release_cli.py
@@ -257,11 +257,219 @@ class TestCreateReleaseCli(IsolatedAsyncioTestCase):
         # Check result
         self.assertEqual(result, Model(expected_release))
 
-    async def test_config_validation_error(self):
-        pass
+    @patch("doozerlib.backend.konflux_client.KonfluxClient.from_kubeconfig")
+    @patch("elliottlib.runtime.Runtime")
+    async def test_config_validation_error(self, mock_runtime, mock_konflux_client_init):
+        mock_runtime.return_value = self.runtime
+        mock_konflux_client_init.return_value = self.konflux_client
 
-    async def test_snapshot_validation_error(self):
-        pass
+        # initialize a regular shipment without a required field i.e. metadata.assembly
+        # so that the shipment schema validation fails
+        shipment_config = {
+            "shipment": {
+                "metadata": {
+                    "product": "ocp",
+                    "application": "openshift-4-18",
+                    "group": "openshift-4.18",
+                    "fbc": True,
+                },
+                "environments": {
+                    "stage": {
+                        "releasePlan": "test-stage-rp",
+                    },
+                    "prod": {
+                        "releasePlan": "test-prod-rp",
+                    }
+                },
+                "snapshot": {
+                    "name": "test-snapshot",
+                    "spec": {
+                        "nvrs": ["test-nvr-1", "test-nvr-2"]
+                    }
+                }
+            }
+        }
+        self.runtime.shipment_gitdata.load_yaml_file.return_value = shipment_config
 
-    async def test_release_plan_not_found(self):
-        pass
+        cli = CreateReleaseCli(
+            runtime=self.runtime,
+            config_path=self.config_path,
+            release_env=self.release_env,
+            konflux_config=self.konflux_config,
+            image_repo_pull_secret=self.image_repo_pull_secret,
+            dry_run=self.dry_run,
+            force=self.force
+        )
+
+        with self.assertRaises(ValueError) as context:
+            await cli.run()
+
+        self.assertIn("1 validation error for ShipmentConfig\nshipment.metadata.assembly\n  Field required", str(context.exception))
+
+    @patch("doozerlib.backend.konflux_client.KonfluxClient.from_kubeconfig")
+    @patch("elliottlib.runtime.Runtime")
+    async def test_config_data_validation_error(self, mock_runtime, mock_konflux_client_init):
+        mock_runtime.return_value = self.runtime
+        mock_konflux_client_init.return_value = self.konflux_client
+
+        # initialize a regular shipment without `data`
+        # so that the shipment schema validation fails
+        shipment_config = {
+            "shipment": {
+                "metadata": {
+                    "product": "ocp",
+                    "application": "openshift-4-18",
+                    "group": "openshift-4.18",
+                    "assembly": "4.18.2",
+                    "fbc": False,
+                },
+                "environments": {
+                    "stage": {
+                        "releasePlan": "test-stage-rp",
+                    },
+                    "prod": {
+                        "releasePlan": "test-prod-rp",
+                    }
+                },
+                "snapshot": {
+                    "name": "test-snapshot",
+                    "spec": {
+                        "nvrs": ["test-nvr-1", "test-nvr-2"]
+                    }
+                }
+            }
+        }
+        self.runtime.shipment_gitdata.load_yaml_file.return_value = shipment_config
+
+        cli = CreateReleaseCli(
+            runtime=self.runtime,
+            config_path=self.config_path,
+            release_env=self.release_env,
+            konflux_config=self.konflux_config,
+            image_repo_pull_secret=self.image_repo_pull_secret,
+            dry_run=self.dry_run,
+            force=self.force
+        )
+
+        with self.assertRaises(ValueError) as context:
+            await cli.run()
+
+        self.assertIn("A regular shipment is expected to have data.releaseNotes defined", str(context.exception))
+
+    @patch("elliottlib.cli.konflux_release_cli.get_utc_now_formatted_str", return_value="timestamp")
+    @patch("doozerlib.backend.konflux_client.KonfluxClient.from_kubeconfig")
+    @patch("elliottlib.cli.konflux_release_cli.GetSnapshotCli")
+    @patch("elliottlib.runtime.Runtime")
+    async def test_shipped_no_force(self, mock_runtime, mock_get_snapshot_cli, mock_konflux_client_init, _):
+        mock_runtime.return_value = self.runtime
+        mock_konflux_client_init.return_value = self.konflux_client
+
+        shipment_config = ShipmentConfig(
+            shipment=Shipment(
+                metadata=Metadata(
+                    product="ocp",
+                    application="openshift-4-18",
+                    group="openshift-4.18",
+                    assembly="4.18.2",
+                    fbc=False,
+                ),
+                environments=Environments(
+                    stage=ShipmentEnv(releasePlan="test-stage-rp"),
+                    prod=ShipmentEnv(
+                        releasePlan="test-prod-rp",
+                        advisoryInternalUrl="foo-bar"
+                    ),
+                ),
+                snapshot=Snapshot(name="test-snapshot", spec=Spec(nvrs=["test-nvr-1", "test-nvr-2"])),
+                data=Data(
+                    releaseNotes=ReleaseNotes(
+                        type="RHBA",
+                        synopsis="Red Hat Openshift Test Release",
+                        topic="Topic for a test release for Red Hat Openshift.",
+                        description="Description for a test release for Red Hat Openshift.",
+                        solution="Solution for a test release for Red Hat Openshift.",
+                    )
+                )
+            )
+        )
+        self.runtime.shipment_gitdata.load_yaml_file.return_value = shipment_config.model_dump(exclude_none=True)
+
+        # Mock API queries
+        self.konflux_client._get_api.return_value = MagicMock()
+        self.konflux_client._get.return_value = MagicMock()
+
+        cli = CreateReleaseCli(
+            runtime=self.runtime,
+            config_path=self.config_path,
+            release_env="prod",
+            konflux_config=self.konflux_config,
+            image_repo_pull_secret=self.image_repo_pull_secret,
+            dry_run=self.dry_run,
+            force=False,
+        )
+
+        with self.assertRaises(ValueError) as context:
+            await cli.run()
+
+        self.assertIn("existing release metadata is not empty for prod", str(context.exception))
+
+    @patch("elliottlib.cli.konflux_release_cli.get_utc_now_formatted_str", return_value="timestamp")
+    @patch("doozerlib.backend.konflux_client.KonfluxClient.from_kubeconfig")
+    @patch("elliottlib.cli.konflux_release_cli.GetSnapshotCli")
+    @patch("elliottlib.runtime.Runtime")
+    async def test_snapshot_validation_error(self, mock_runtime, mock_get_snapshot_cli, mock_konflux_client_init, _):
+        mock_runtime.return_value = self.runtime
+        mock_konflux_client_init.return_value = self.konflux_client
+
+        shipment_config = ShipmentConfig(
+            shipment=Shipment(
+                metadata=Metadata(
+                    product="ocp",
+                    application="openshift-4-18",
+                    group="openshift-4.18",
+                    assembly="4.18.2",
+                    fbc=False,
+                ),
+                environments=Environments(
+                    stage=ShipmentEnv(releasePlan="test-stage-rp"),
+                    prod=ShipmentEnv(releasePlan="test-prod-rp"),
+                ),
+                snapshot=Snapshot(name="test-snapshot", spec=Spec(nvrs=["test-nvr-1", "test-nvr-2"])),
+                data=Data(
+                    releaseNotes=ReleaseNotes(
+                        type="RHBA",
+                        synopsis="Red Hat Openshift Test Release",
+                        topic="Topic for a test release for Red Hat Openshift.",
+                        description="Description for a test release for Red Hat Openshift.",
+                        solution="Solution for a test release for Red Hat Openshift.",
+                    )
+                )
+            )
+        )
+        self.runtime.shipment_gitdata.load_yaml_file.return_value = shipment_config.model_dump(exclude_none=True)
+
+        # Mock API queries
+        self.konflux_client._get_api.return_value = MagicMock()
+        self.konflux_client._get.return_value = MagicMock()
+
+        # Mock snapshot verification
+        mock_get_snapshot_instance = AsyncMock()
+        # not all NVRs are present in snapshot
+        mock_get_snapshot_instance.run.return_value = ["test-nvr-1", "test-nvr-3"]
+        mock_get_snapshot_cli.return_value = mock_get_snapshot_instance
+
+        cli = CreateReleaseCli(
+            runtime=self.runtime,
+            config_path=self.config_path,
+            release_env=self.release_env,
+            konflux_config=self.konflux_config,
+            image_repo_pull_secret=self.image_repo_pull_secret,
+            dry_run=self.dry_run,
+            force=self.force
+        )
+
+        with self.assertRaises(ValueError) as context:
+            await cli.run()
+
+        self.assertIn("snapshot includes missing or extra nvrs than what's defined in spec: missing={'test-nvr-2'} "
+                      "extra={'test-nvr-3'}", str(context.exception))
