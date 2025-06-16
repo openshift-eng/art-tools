8efe4425dd2ad5d071938aeb7b196cce3942d9c4
Siddharth Sharma <sidsharm@redhat.com>
2025-06-16 08:47:46 -0400

    remove comment

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # Requires a local virtualenv at ./.venv. This can be created with "uv venv --python 3.11"
-# Ignore .egg-info dirs in repo that might exist
 uv pip install -e artcommon/ -e doozer/ -e elliott/ -e pyartcd/ -e ocp-build-data-validator/
