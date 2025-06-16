5f287c42c6db1c7ff660be508f421d366dc4950e
Siddharth Sharma <sidsharm@redhat.com>
2025-06-16 08:43:53 -0400

    rebase

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Requires a local virtualenv at ./.venv. This can be created with "uv venv --python 3.11"
-uv pip install -e artcommon/ -e doozer/ -e elliott/ -e pyartcd/ -e ocp-build-data-validator/
+# Ignore .egg-info dirs in repo that might exist
+uv --no-cache pip install -e artcommon/ -e doozer/ -e elliott/ -e pyartcd/ -e ocp-build-data-validator/
