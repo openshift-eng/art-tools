2277b712d69a9d938c13f78dbd76d03a7a88edf3
Siddharth Sharma <sidsharm@redhat.com>
2025-07-02 17:02:33 -0400

    Support build-data in tools shipment schema

--- elliott/elliottlib/shipment_model.py
+++ elliott/elliottlib/shipment_model.py
@@ -137,6 +137,7 @@ class Tools(StrictBaseModel):
     """Tools to use when releasing shipment to an environment"""
 
     art_tools: Optional[str] = Field(None, alias="art-tools")
+    build_data: Optional[str] = Field(None, alias="build-data")
 
 
 class Shipment(StrictBaseModel):
