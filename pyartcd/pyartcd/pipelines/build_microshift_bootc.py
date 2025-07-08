19ea6f7ce5e49aaf65260fe1a21fa0159e67d81e
Siddharth Sharma <sidsharm@redhat.com>
2025-07-08 14:58:07 -0400

    Replace basic auth with registry config

--- pyartcd/pyartcd/pipelines/build_microshift_bootc.py
+++ pyartcd/pyartcd/pipelines/build_microshift_bootc.py
@@ -98,7 +98,7 @@ class BuildMicroShiftBootcPipeline:
             'login',
             '--registry',
             'quay.io/redhat-user-workloads',
-            f'--auth-basic={os.environ["KONFLUX_ART_IMAGES_USERNAME"]}:{os.environ["KONFLUX_ART_IMAGES_PASSWORD"]}',
+            f'--registry-config={os.getenv("KONFLUX_ART_IMAGES_AUTH_FILE")}',
         ]
         await exectools.cmd_assert_async(cmd)
 
