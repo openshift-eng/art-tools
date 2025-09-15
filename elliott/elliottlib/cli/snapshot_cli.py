e4065b06c20f3abd5d0de2dcd3ea38cceda4e65a
D. Paolella <dpaolell@redhat.com>
2025-09-15 16:31:17 +0200

    Elliot snapshot new: handle images ending with 'metadata'

--- elliott/elliottlib/cli/snapshot_cli.py
+++ elliott/elliottlib/cli/snapshot_cli.py
@@ -66,7 +66,9 @@ async def get_build_records_by_nvrs(runtime: Runtime, nvrs: list[str]) -> dict[s
     type_nvrs = defaultdict(list)
     for nvr in nvrs:
         nvr_dict = parse_nvr(nvr)
-        if nvr_dict['name'].endswith('-bundle-container') or nvr_dict['name'].endswith('-metadata-container'):
+        if nvr_dict['name'].replace('-container', '') in runtime.image_map:
+            type_nvrs[KonfluxBuildRecord].append(nvr)
+        elif nvr_dict['name'].endswith('-bundle-container') or nvr_dict['name'].endswith('-metadata-container'):
             type_nvrs[KonfluxBundleBuildRecord].append(nvr)
         elif nvr_dict['name'].endswith('-fbc'):
             type_nvrs[KonfluxFbcBuildRecord].append(nvr)
