89482b6bb5bc0076cfe3dd3affb77c17d0691488
Ashwin Das <ashwindasr@gmail.com>
2025-09-15 09:38:46 -0400

    allow non-openshift group for fbc

--- pyartcd/pyartcd/pipelines/build_fbc.py
+++ pyartcd/pyartcd/pipelines/build_fbc.py
@@ -2,7 +2,6 @@ import logging
 from datetime import datetime, timezone
 from pathlib import Path
 from typing import List, Optional
-import re
 import click
 from artcommonlib import exectools
 
@@ -30,10 +29,12 @@ class BuildFbcPipeline:
         reset_to_prod: bool,
         prod_registry_auth: Optional[str],
         force: bool,
+        group: str
     ):
         self.runtime = runtime
         self.version = version
         self.assembly = assembly
+        self.group = group
         self.data_path = data_path
         self.data_gitref = data_gitref
         self.only = only
@@ -75,16 +76,12 @@ class BuildFbcPipeline:
             raise
 
     async def _run_doozer(self, opts: List[str], only: str, exclude: str):
-        if re.match(r'^\d', self.version):
-            group = f"openshift-{self.version}"
-        else:
-            group = self.version
         cmd = [
             'doozer',
             '--build-system=konflux',
             f'--working-dir={self.runtime.doozer_working}',
             f'--assembly={self.assembly}',
-            f'--group={group}{"@" + self.data_gitref if self.data_gitref else ""}',
+            f'--group={self.group}{"@" + self.data_gitref if self.data_gitref else ""}',
         ]
         if self.data_path:
             cmd.append(f'--data-path={self.data_path}')
@@ -178,6 +175,13 @@ class BuildFbcPipeline:
     default=constants.OCP_BUILD_DATA_URL,
     help='ocp-build-data fork to use (e.g. assembly definition in your own fork)',
 )
+@click.option(
+    "-g",
+    "--group",
+    metavar='NAME',
+    required=True,
+    help="The group of components on which to operate. e.g. openshift-4.9",
+)
 @click.option('--data-gitref', required=False, help='(Optional) Doozer data path git [branch / tag / sha] to use')
 @click.option(
     '--only',
@@ -235,6 +239,7 @@ async def build_fbc(
     reset_to_prod: bool,
     prod_registry_auth: Optional[str],
     force: bool,
+    group: str,
 ):
     pipeline = BuildFbcPipeline(
         runtime=runtime,
@@ -252,5 +257,6 @@ async def build_fbc(
         reset_to_prod=reset_to_prod,
         prod_registry_auth=prod_registry_auth,
         force=force,
+        group=group
     )
     await pipeline.run()
