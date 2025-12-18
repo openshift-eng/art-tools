7e24cd61251eda8c15f404f561310b557b7f3e40
Fabio Gallotti <fgallott@redhat.com>
2025-12-18 11:30:41 +0100

    ART-14309: single pyproject.toml

--- elliott/elliottlib/__init__.py
+++ elliott/elliottlib/__init__.py
@@ -2,15 +2,15 @@ import sys
 from importlib.metadata import PackageNotFoundError, version
 from typing import cast
 
-if sys.version_info < (3, 8):
-    sys.exit('Sorry, Python < 3.8 is no longer supported.')
+if sys.version_info < (3, 11):
+    sys.exit('Sorry, Python < 3.11 is no longer supported.')
 
 from elliottlib.runtime import Runtime
 
 __version__ = "0.0.0"
 
 try:
-    __version__ = cast(str, version("rh-elliott"))
+    __version__ = cast(str, version("rh-art-tools"))
 except PackageNotFoundError:
     # package is not installed
     pass
