c7e1d270f44d0bda6be84d43b19b2ef37c376bf3
Justin Pierce <jupierce@redhat.com>
2026-01-05 19:50:50 -0500

    Reapply "Fixing inefficient queries to bigquery"
    This reverts commit efbb2c7edf1efb36f7b86bf1404b088d02dd3901.

--- doozer/doozerlib/cli/release_gen_assembly.py
+++ doozer/doozerlib/cli/release_gen_assembly.py
@@ -408,6 +408,7 @@ class GenAssemblyCli:
                 build_record = await self.runtime.konflux_db.get_build_record_by_nvr(
                     nvr=build_nvr,
                     outcome=KonfluxBuildOutcome.SUCCESS,
+                    exclude_large_columns=True,
                 )
                 build_inspector = KonfluxBuildRecordInspector(self.runtime, build_record)
 
