77a8caf80690454cee350e0d213320245cbdd649
Siddharth Sharma <sidsharm@redhat.com>
2026-01-08 12:01:17 -0500

    Remove comment

--- pyartcd/pyartcd/pipelines/rebuild_golang_rpms.py
+++ pyartcd/pyartcd/pipelines/rebuild_golang_rpms.py
@@ -174,11 +174,6 @@ class RebuildGolangRPMsPipeline:
         return [c['name'].removesuffix('.yml') for c in content if c['name'].endswith('.yml')]
 
     def rebuild_art_rpms(self, rpms):
-        # _LOGGER.info(
-        #     f"Trigger job at {constants.JENKINS_UI_URL}/job/aos-cd-builds/job/build%252Focp4/build "
-        #     f"with params BUILD_VERSION={self.ocp_version} ASSEMBLY=stream "
-        #     f"BUILD_IMAGES=none BUILD_RPMS=only RPM_LIST={','.join(rpms)}"
-        # )
         jenkins.start_ocp4_konflux(
             build_version=self.ocp_version,
             assembly='stream',
