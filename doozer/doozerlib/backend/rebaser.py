b8c643f7bf0eaf8530a17787c34ae8477fe651e9
ximhan <ximhan@redhat.com>
2026-02-10 15:54:14 +0800

    use multi arch digest

--- doozer/doozerlib/backend/rebaser.py
+++ doozer/doozerlib/backend/rebaser.py
@@ -2480,7 +2480,10 @@ class KonfluxRebaser:
             try:
                 registry_auth = os.getenv("KONFLUX_OPERATOR_INDEX_AUTH_FILE")
                 image_info = await util.oc_image_info_for_arch_async__caching(target, registry_config=registry_auth)
-                digest = image_info.get('digest')
+                # Use listDigest for multi-arch images, if no listDigest, use digest
+                digest = image_info.get('listDigest')
+                if not digest:
+                    digest = image_info.get('digest')
             except Exception as e:
                 self._logger.error(f"Failed to resolve digest for {target}: {e}")
                 raise
