c0bf28d17dba6d1ea6a353c095c03d350406b0c0
Siddharth Sharma <sidsharm@redhat.com>
2026-04-16 12:19:14 -0400

    Allow excluding components in assembly definition (#2565)
    * Add exclude key to exclude components
    
    * add rpm support
    
    * Add schema and tests to validator
    
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- ocp-build-data-validator/tests/test_releases.py
+++ ocp-build-data-validator/tests/test_releases.py
@@ -2,6 +2,7 @@ import unittest
 
 from flexmock import flexmock
 from validator import releases
+from validator.schema import releases_schema
 
 group_config = group_cfg = {
     'vars': {'MAJOR': 4, 'MINOR': 18},
@@ -33,3 +34,145 @@ class TestReleases(unittest.TestCase):
         }
         err = releases.validate(valid_releases)
         self.assertIsNone(err)
+
+    def test_member_exclude_image(self):
+        valid_releases = {
+            "releases": {
+                "4.18.1": {
+                    "assembly": {
+                        "members": {
+                            "images": [
+                                {"distgit_key": "image-a", "exclude": True, "why": "not needed for this release"},
+                                {"distgit_key": "image-c", "why": "pinned build", "metadata": {}},
+                            ]
+                        }
+                    }
+                }
+            }
+        }
+        err = releases_schema.validate(None, valid_releases)
+        self.assertIsNone(err)
+
+    def test_member_exclude_rpm(self):
+        valid_releases = {
+            "releases": {
+                "4.18.1": {
+                    "assembly": {
+                        "members": {
+                            "rpms": [
+                                {"distgit_key": "rpm-a", "exclude": True, "why": "not needed for this release"},
+                                {
+                                    "distgit_key": "rpm-c",
+                                    "why": "pinned build",
+                                    "metadata": {"is": {"el9": "rpm-c-1.0-1.el9"}},
+                                },
+                            ]
+                        }
+                    }
+                }
+            }
+        }
+        err = releases_schema.validate(None, valid_releases)
+        self.assertIsNone(err)
+
+    def test_member_exclude_false_image_invalid(self):
+        invalid_releases = {
+            "releases": {
+                "4.18.1": {
+                    "assembly": {
+                        "members": {
+                            "images": [
+                                {"distgit_key": "image-a", "exclude": False, "why": "reason"},
+                            ]
+                        }
+                    }
+                }
+            }
+        }
+        err = releases_schema.validate(None, invalid_releases)
+        self.assertIsNotNone(err)
+
+    def test_member_exclude_false_rpm_invalid(self):
+        invalid_releases = {
+            "releases": {
+                "4.18.1": {
+                    "assembly": {
+                        "members": {
+                            "rpms": [
+                                {"distgit_key": "rpm-a", "exclude": False, "why": "reason"},
+                            ]
+                        }
+                    }
+                }
+            }
+        }
+        err = releases_schema.validate(None, invalid_releases)
+        self.assertIsNotNone(err)
+
+    def test_member_image_why_required_with_exclude(self):
+        invalid_releases = {
+            "releases": {
+                "4.18.1": {
+                    "assembly": {
+                        "members": {
+                            "images": [
+                                {"distgit_key": "image-a", "exclude": True},
+                            ]
+                        }
+                    }
+                }
+            }
+        }
+        err = releases_schema.validate(None, invalid_releases)
+        self.assertIsNotNone(err)
+
+    def test_member_rpm_why_required_with_exclude(self):
+        invalid_releases = {
+            "releases": {
+                "4.18.1": {
+                    "assembly": {
+                        "members": {
+                            "rpms": [
+                                {"distgit_key": "rpm-a", "exclude": True},
+                            ]
+                        }
+                    }
+                }
+            }
+        }
+        err = releases_schema.validate(None, invalid_releases)
+        self.assertIsNotNone(err)
+
+    def test_member_image_why_required(self):
+        invalid_releases = {
+            "releases": {
+                "4.18.1": {
+                    "assembly": {
+                        "members": {
+                            "images": [
+                                {"distgit_key": "image-a"},
+                            ]
+                        }
+                    }
+                }
+            }
+        }
+        err = releases_schema.validate(None, invalid_releases)
+        self.assertIsNotNone(err)
+
+    def test_member_rpm_why_required(self):
+        invalid_releases = {
+            "releases": {
+                "4.18.1": {
+                    "assembly": {
+                        "members": {
+                            "rpms": [
+                                {"distgit_key": "rpm-a"},
+                            ]
+                        }
+                    }
+                }
+            }
+        }
+        err = releases_schema.validate(None, invalid_releases)
+        self.assertIsNotNone(err)
