af92dd481504ec16565ebf709fd914abd5736b1a
RayfordJ <4580787+rayfordj@users.noreply.github.com>
2026-05-29 14:23:29 -0500

    Revert "Temporarily skip RPMs in ocp4-scan-konflux due to distgit connection issues"

--- pyartcd/pyartcd/pipelines/ocp4_scan_konflux.py
+++ pyartcd/pyartcd/pipelines/ocp4_scan_konflux.py
@@ -100,7 +100,6 @@ class Ocp4ScanPipeline:
                 '--yaml',
                 f'--ci-kubeconfig={os.environ["KUBECONFIG"]}',
                 '--rebase-priv',
-                '--skip-rpms',
             ]
         )
         if self.runtime.dry_run:
